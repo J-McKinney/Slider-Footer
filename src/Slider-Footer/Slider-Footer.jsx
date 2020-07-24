09b3bc44779ec480766b9364a7d1961c04465e62
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-24 14:25:52 -0400

    yay

--- src/Slider-Footer/Slider-Footer.jsx
+++ src/Slider-Footer/Slider-Footer.jsx
@@ -17,32 +17,72 @@ class SkinnyFooter extends Component {
             <Col>
               <ul>
                 <li>
-                  <h5>The World Of React 1</h5>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-1">The World Of React 1</a>
+                  </h5>
                 </li>
                 <li>
-                  <h5>The World Of React 2</h5>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-2">The World Of React 2</a>
+                  </h5>
                 </li>
                 <li>
-                  <h5>The World Of React 3</h5>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-3">The World Of React 3</a>
+                  </h5>
                 </li>
                 <li>
-                  <h5>The World Of React 4</h5>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-4">The World Of React 4</a>
+                  </h5>
                 </li>
               </ul>
             </Col>
             <Col>
               <ul>
                 <li>
-                  <h5>The World Of React 5</h5>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-5">The World Of React 5</a>
+                  </h5>
                 </li>
                 <li>
-                  <h5>The World Of React 6</h5>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-6">The World Of React 6</a>
+                  </h5>
                 </li>
                 <li>
-                  <h5>The World Of React 7</h5>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-7">The World Of React 7</a>
+                  </h5>
                 </li>
                 <li>
-                  <h5>The World Of React 8</h5>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-8">The World Of React 8</a>
+                  </h5>
+                </li>
+              </ul>
+            </Col>
+            <Col>
+              <ul>
+                <li>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-9">The World Of React 9</a>
+                  </h5>
+                </li>
+                <li>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-10">The World Of React 10</a>
+                  </h5>
+                </li>
+                <li>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-11">The World Of React 11</a>
+                  </h5>
+                </li>
+                <li>
+                  <h5>
+                    <a className="bottomFooterLinks" href="#/action-12">The World Of React 12</a>
+                  </h5>
                 </li>
               </ul>
             </Col>
